42a083b89f0e425affb2bafe96eaf268e7f8b576
saipradeepvarry <saipradeepvarry79979@gmail.com>
2024-05-27 19:22:40 +0530

    Resolved merge conflicts

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Main from './Main';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Main.js
@@ -0,0 +1,359 @@
+// Main.js
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='flex-row-ee'>
+        <div className='vector'>
+          <div className='pink-header-bg'>
+            <div className='vector-1' />
+            <div className='vector-2' />
+            <div className='group' />
+            <div className='vector-3' />
+            <span className='follow-us'>Follow Us</span>
+          </div>
+        </div>
+        <div className='group-4' />
+        <div className='background-shape'>
+          <div className='vector-5' />
+          <div className='vector-6' />
+          <div className='rt' />
+        </div>
+        <div className='hero-mock'>
+          <span className='home-about-us-features-contact'>
+            Home About Us Features Contact
+          </span>
+          <div className='user' />
+          <div className='group-7'>
+            <div className='clip-path-group' />
+          </div>
+        </div>
+        <span className='best-mobile-app-showcase'>
+          Best Mobile <br />
+          App Showcase
+        </span>
+        <div className='shape' />
+        <span className='aenean-dictum-odio'>
+          Aenean dictum odio dapibus turpis dapibus vestibulum. Mauris quis
+          massa <br />
+          nisi. Nullam porta lorem at volutpat euismod. Proin in ex nunc.
+        </span>
+        <button className='rectangle'>
+          <span className='read-more'>Read More</span>
+        </button>
+        <div className='group-8'>
+          <div className='flex-row-c'>
+            <div className='girl' />
+            <span className='engaging-spacious-school-campus'>
+              Engaging & Spacious <br />
+              School Campus
+            </span>
+            <div className='dsd' />
+            <div className='dsd-9' />
+            <span className='hise-sed-augue'>
+              Hise sed augue vitae felis pellentesque varius nec quis nunc.
+              Morbi mauris augue, pulvinar quis luctus eget. Phasellus gravida
+              lacus quis eros lobortis, nec dapibus quam gravida.
+            </span>
+          </div>
+          <div className='flex-row-afd'>
+            <div className='vector-a' />
+            <div className='vector-b' />
+            <div className='vector-c'>
+              <span className='perfect-ui-design'>Perfect UI Design</span>
+              <div className='ellipse' />
+              <div className='content' />
+              <div className='vector-d' />
+              <div className='group-e' />
+              <span className='ac-vehicula-sapien'>
+                Praesent ac vehicula sapien. Sed sollicitudin molestie
+                consequat. Ut vitae ante ut mi vehicula vulputate.
+              </span>
+              <div className='ellipse-f' />
+              <div className='vector-10' />
+            </div>
+            <div className='vector-11'>
+              <span className='perfect-ui-design-12'>Perfect UI Design</span>
+              <div className='ellipse-13' />
+              <div className='content-14' />
+              <div className='vector-15' />
+              <div className='group-16' />
+              <span className='ac-vehicula-sapien-17'>
+                Praesent ac vehicula sapien. Sed sollicitudin molestie
+                consequat. Ut vitae ante ut mi vehicula vulputate.
+              </span>
+              <div className='ellipse-18' />
+              <div className='vector-19' />
+            </div>
+          </div>
+          <span className='easy-perfect-solution'>
+            Easy And Perfect Solution For <br />
+            Your Business App
+          </span>
+          <div className='vector-1a' />
+          <div className='flex-row-f'>
+            <span className='aliquam-aliquet-purus'>
+              Aliquam aliquet purus a est consequat lobortis. Etiam vehicula
+              suscipit purus, eget ullamcorper augue blandit vitae. Ut eu
+              euismod felis. Etiam at varius quam. Vivamus lacinia pulvinar
+              turpis in suscipit. Aenean mattis enim ut pretium gravida. Sed
+              fermentum a lacus bibendum convallis.
+              <br />
+              <br />
+              Consequat purus aliquet a est aliquam lobortis. Etiam vehicula
+              suscipit purus, eget ullamcorper augue blandit vitae....
+            </span>
+            <button className='rectangle-1b'>
+              <span className='read-more-1c'>Read More</span>
+            </button>
+          </div>
+        </div>
+        <div className='vector-1d' />
+        <div className='vector-1e'>
+          <span className='perfect-ui-design-1f'>Perfect UI Design</span>
+          <div className='ellipse-20' />
+          <div className='content-21' />
+          <div className='vector-22' />
+          <div className='group-23' />
+          <span className='ac-vehicula-sapien-24'>
+            Praesent ac vehicula sapien. Sed sollicitudin molestie consequat. Ut
+            vitae ante ut mi vehicula vulputate.
+          </span>
+          <div className='ellipse-25' />
+          <div className='vector-26' />
+        </div>
+        <div className='dsd-27' />
+        <div className='xc' />
+        <div className='removebg-preview' />
+        <div className='zas' />
+      </div>
+      <div className='rectangle-28'>
+        <div className='flex-column-c'>
+          <span className='how-app-work'>How does This App Work?</span>
+          <div className='dsd-29' />
+          <div className='rectangle-2a'>
+            <span className='make-profile'>Make A Profile</span>
+            <div className='group-2b' />
+          </div>
+          <span className='aliquam-varius-ligula'>
+            Aliquam varius ligula nec leo tempus porta. Vestibulum suscipit leo
+            at nunc imperdiet, quis lacinia nisi euismod.
+          </span>
+          <div className='rectangle-2c'>
+            <span className='make-a-profile'>Make A Profile</span>
+            <div className='ellipse-2d' />
+            <div className='ellipse-2e' />
+            <div className='ellipse-2f' />
+            <div className='vector-30' />
+          </div>
+          <span className='tempus-porta-vestibulum'>
+            Aliquam varius ligula nec leo tempus porta. Vestibulum suscipit leo
+            at nunc imperdiet, quis lacinia nisi euismod.
+          </span>
+          <div className='rectangle-31'>
+            <span className='make-a-profile-32'>Make A Profile</span>
+            <div className='ellipse-33' />
+            <div className='ellipse-34' />
+            <div className='ellipse-35' />
+            <div className='vector-36' />
+          </div>
+          <span className='tempus-porta-vestibulum-37'>
+            Aliquam varius ligula nec leo tempus porta. Vestibulum suscipit leo
+            at nunc imperdiet, quis lacinia nisi euismod.
+          </span>
+        </div>
+        <div className='removebg-preview-38' />
+      </div>
+      <div className='flex-row-cf'>
+        <div className='sa' />
+        <div className='sa-39' />
+        <div className='freelance-man-is-working-removebg-preview' />
+        <span className='designed-worked-latest-partners'>
+          Designed & Worked By The Latest <br />
+          Partners
+        </span>
+        <button className='rectangle-3a'>
+          <span className='tempus-porta'>
+            Aliquam varius ligula nec leo tempus porta.
+          </span>
+        </button>
+        <div className='rectangle-3b'>
+          <span className='lorem-ipsum-dummy'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+          </span>
+        </div>
+        <div className='rectangle-3c'>
+          <span className='suspendisse-vitae-varius'>
+            Suspendisse vitae varius diam, a vulputate urna.
+          </span>
+        </div>
+        <button className='rectangle-3d'>
+          <span className='aliquam-aliquet-purus-3e'>
+            Aliquam aliquet purus eget lacus pretium.
+          </span>
+        </button>
+      </div>
+      <div className='rectangle-3f'>
+        <span className='app-screenshots'>App Screenshots</span>
+        <div className='dsd-40' />
+        <div className='flex-row-d'>
+          <span className='saepe-quolabore-aenean'>
+            Saepe quo labore aenean dictumst expedita commodi auctor, nisl,
+            lorem iusto feugiat nemo reiciendis laboris.
+          </span>
+          <div className='phone-mokeup'>
+            <div className='screenshot' />
+          </div>
+          <div className='screenshot-41' />
+          <div className='screenshot-42' />
+          <div className='screenshot-43' />
+          <div className='screenshot-44' />
+        </div>
+        <div className='flex-row-c-45'>
+          <div className='vector-46' />
+          <div className='vector-47' />
+          <div className='vector-48' />
+        </div>
+      </div>
+      <div className='flex-row-a'>
+        <div className='rectangle-49'>
+          <span className='reviews-from-students'>Review's From Students</span>
+          <div className='dsd-4a' />
+          <div className='flex-row-ab'>
+            <div className='rectangle-4b'>
+              <div className='flex-row-fafe'>
+                <span className='per-sed-mattis'>
+                  Per sed, mattis. Integer viverra euismod maecenas incidunt,
+                  phasellus consequatur aliquam nihil temporibus in assumenda?
+                  Aute praesentium fugiat. Perspiciatis, ultrices deserunt
+                  convallis, eius at non.
+                </span>
+                <div className='aro' />
+                <div className='vector-4c' />
+                <div className='inverted-commas-top' />
+                <span className='jenny-wilson'>Jenny Wilson</span>
+                <div className='vector-4d' />
+                <div className='vector-4e' />
+                <div className='vector-4f' />
+                <div className='vector-50' />
+                <div className='vector-51' />
+              </div>
+              <div className='flex-row-d-52'>
+                <div className='mask-group' />
+                <div className='ellipse-53' />
+              </div>
+            </div>
+            <div className='rectangle-54'>
+              <div className='flex-row-fb'>
+                <span className='per-sed-mattis-55'>
+                  Per sed, mattis. Integer viverra euismod maecenas incidunt,
+                  phasellus consequatur aliquam nihil temporibus in assumenda?
+                  Aute praesentium fugiat. Perspiciatis, ultrices deserunt
+                  convallis, eius at non.
+                </span>
+                <div className='aro-56' />
+                <div className='vector-57' />
+                <div className='inverted-commas-top-58' />
+                <span className='jenny-wilson-59'>Jenny Wilson</span>
+                <div className='vector-5a' />
+                <div className='vector-5b' />
+                <div className='vector-5c' />
+                <div className='vector-5d' />
+                <div className='vector-5e' />
+              </div>
+              <div className='flex-row'>
+                <div className='mask-group-5f' />
+                <div className='ellipse-60' />
+              </div>
+            </div>
+            <div className='vector-61' />
+            <div className='vector-62' />
+          </div>
+        </div>
+        <div className='rectangle-63'>
+          <div className='vector-64' />
+          <div className='flex-column-ae'>
+            <span className='subscribe-our-newsletter'>
+              Subscribe Our Newsletter !
+            </span>
+            <div className='vector-65' />
+            <span className='elementum-ex-similique'>
+              Elementum ex similique sollicitudin eveniet sem eveniet proin,
+              iste euismod! Quia! Fugiat molestie leo placerat, tenetur.
+            </span>
+          </div>
+          <div className='rectangle-66'>
+            <button className='rectangle-67'>
+              <span className='subscribe'>SUBSCRIBE</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className='rectangle-68'>
+        <span className='quick-links-useful-links'>
+          Quick Links Useful Links School Hours
+        </span>
+        <div className='flex-row-fc'>
+          <div className='group-69' />
+          <div className='group-6a' />
+          <div className='group-6b' />
+        </div>
+        <div className='flex-row-6c'>
+          <span className='home-about-us-careers'>
+            Home <br />
+            About us
+            <br />
+            Careers
+            <br />
+            News & Articles
+          </span>
+          <span className='help-center-contact-us'>
+            Help Center
+            <br />
+            Contact Us
+            <br />
+            FAQ
+            <br />
+            Parent Community
+          </span>
+          <span className='school-hours'>8 AM - 5 PM , Monday - Saturday</span>
+          <div className='vector-6d' />
+          <div className='vector-6e' />
+          <div className='vector-6f' />
+          <span className='aut-quae-convallis'>
+            Aut, quae convallis minim cum ornare! Pede ut rem, totam dictum
+            convallis.
+          </span>
+          <div className='group-70' />
+          <div className='vector-71' />
+          <div className='vector-72' />
+          <span className='vero-tempor-morbi'>
+            Vero, tempor morbi, adipiscing aliqua nonummy proident perferendis?
+            Laboris lacus quidem repellendus quasi.
+          </span>
+          <div className='vector-73' />
+          <div className='vector-74' />
+          <div className='group-75' />
+          <div className='group-76' />
+          <div className='group-77' />
+          <div className='vector-78' />
+          <div className='vector-79' />
+        </div>
+        <div className='rectangle-7a'>
+          <span className='copyright'>
+            Copyright © 2022 Educator. All rights reserved.
+          </span>
+          <span className='privacy-policy-support'>
+            PRIVACY POLICY | SUPPORT | TERMS & CONDITION
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
